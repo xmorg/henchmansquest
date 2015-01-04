c34f310552596d01043d53c411a0cf6d1fd60cc6
Tim <jobs@cooperlabs.net>
2015-01-04 07:19:12 -0800

    tile loading code

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
-sprite_quad = {}
-wall_quad   = {}--10x,5y, 320x320
--- 10 tiles wide, 20 tiles tall, 320x320
+tile_images = {}
+wall_images = {}
+
 ground_tiles_x = 10
 ground_tiles_y = 20
 ground_pic_size_x = 320
@@ -8,43 +8,22 @@ ground_pic_size_y = 320
 ground_quad = {}         
 
 game = {
-	player_loc_x =0
-	player_loc_y =0
-	draw_y =0
+	player_loc_x =0,
+	player_loc_y =0,
+	draw_y =0,
 	draw_x =0
 }
 
-require("draw")
+--require("draw")
 
 function love.load()
-	--tiles	--love.graphics.newQuad( x, y, width, height, sw, sh )
-	x32_landTiles_iso = love.graphics.newImage("data/x32_landTiles_iso.png")
-	x32_wallTiles_iso = love.graphics.newImage("data/x32_wallTiles_iso.png")
-	--for height / tiles
-	
-	for y = 1, 20 do
-		for x  = 1, 10 do
-			table.insert(ground_quad, love.graphics.newQuad(x*32, y*16, 32, 16, 320,320) )
-		end
-	end
-	for y = 1,5 do
-		for x = 1,10 do
-			table.insert(wall_quad, love.graphics.newQuad(x * 32, y * 64, 64,32,320,320) )
-		end
-	end
-	--dark elves
-	darkelf_female32x32 = love.graphics.newImage("data/darkelf_female32x32.png")
-	darkelf_male32x32 = love.graphics.newImage("data/darkelf_male32x32.png")
-	--dwarves
-	dwarf_female_32 = love.graphics.newImage("data/dwarf_female_32.png")
-	dwarf_male_32 = love.graphics.newImage("data/dwarf_male_32.png")
-	--humans
-	human_female32x32 = love.graphics.newImage("data/human_female32x32.png")
-	human_male32x32   = love.graphics.newImage("data/human_male32x32.png")
-	
-	--elf
-	elf_female_32 = love.graphics.newImage("data/elf_female_32.png")
-	elf_male32x32 = love.graphics.newImage("data/elf_male32x32.png")
+	--tiles
+	table.insert(tile_images,love.graphics.newImage("data/tiles/tile001.png") )
+	table.insert(tile_images,love.graphics.newImage("data/tiles/tile002.png") )
+	--walls
+	table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall001.png") )
+	--sprites
+	--UI
 end
 
 function love.update()
