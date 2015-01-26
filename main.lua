25c1d8eb6d78726667ed329042e1d8e9e0f51943
xmorg <jobs@cooperlabs.net>
2015-01-26 07:30:20 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -53,7 +53,7 @@ function love.load()
    menu_scroll = love.graphics.newImage("data/ui/title_scroll.png")
    title_menu_text = love.graphics.newImage("data/ui/title_menu_text.png")
    on_load_tiles()
-   --game.player = create_actor_blank()
+   game.player = create_actor_blank()
 end
 
 function love.update()
