cb72185a7274725c3724a01c2748fe83efb458aa
xmorg <jobs@cooperlabs.net>
2015-02-25 09:56:26 -0800

    Update tactical.lua

--- tactical.lua
+++ tactical.lua
@@ -1,6 +1,7 @@
 --tactical battle map
 
 function tactical_loop(setting, team1, team2)
+  battle_order={}
   if setting == 0 then
     while(team1_initiative_roll ~= team2_initiative_roll) do
       team1_initiative_roll = math.random(1,6)
@@ -10,7 +11,11 @@ function tactical_loop(setting, team1, team2)
     end
     if team1_initiative_roll > team2_initiative_roll then
       --start with team 1
+      table.insert(battle_order, team1)
+      table.insert(battle_order, team2)
     else -- team1_initiative_roll < team2_initiative_roll
+      table.insert(battle_order, team2)
+      table.insert(battle_order, team1)
       --start with team 2
   elseif setting == 1 then
     --start with team 1
