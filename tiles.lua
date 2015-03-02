7c65fff6f0a9ea3a32c7d3d0db7bdba23abe983b
Tim <jobs@cooperlabs.net>
2015-03-02 04:31:44 -0800

    make tiles scalable

--- tiles.lua
+++ tiles.lua
@@ -1,3 +1,6 @@
+
+last_tile = 0
+
 function new_game_map()
    for y = 1, game.tilecount do
       row = {}
@@ -5,7 +8,7 @@ function new_game_map()
       table.insert(tile_map, row)
       table.insert(wall_map, wallrow)
       for x = 1, game.tilecount do
-	 table.insert(tile_map[y], 1)
+	 table.insert(tile_map[y], 9)
 	 table.insert(wall_map[y], 1)
       end
    end
@@ -27,8 +30,10 @@ function on_load_tiles() --load the tiles
    table.insert(tile_images,love.graphics.newImage("data/tiles/tile005.png") )
    table.insert(tile_images,love.graphics.newImage("data/tiles/tile006.png") )
    table.insert(tile_images,love.graphics.newImage("data/tiles/tile007.png") )
-
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile008.png") )
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile009.png") )
    table.insert(tile_images,love.graphics.newImage("data/tiles/tile999.png") )
+   
    --walls
    table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall001.png") )
    table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall002.png") )
@@ -37,9 +42,21 @@ function on_load_tiles() --load the tiles
    table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall005.png") )
    table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall006.png") )
    table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall007.png") )
+
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile999.png") )
+   
+   
+   
    --table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall008.png") 
    --sprites
    --UI
+
+   function tablelength(T)
+      local count = 0
+      for _ in pairs(T) do count = count + 1 end
+      return count
+   end
+   last_tile = tablelength(tile_images)
    
    if game.fullscreen_hack == "yes" then
       if game.version == "0.8.0" then 
@@ -94,7 +111,7 @@ function draw_select_grid()
 	 tile_x = game.draw_x+(y + x) * 125 +250  --250 + 125
 	 tile_y = game.draw_y+(y - x) * 125 /2 + (129/2) --129 / 2 + 64
 	 if y == game.tile_selected_y and x == game.tile_selected_x then
-	    love.graphics.draw(tile_images[8], tile_x+game.draw_x, tile_y+game.draw_y, 0)
+	    love.graphics.draw(tile_images[last_tile], tile_x+game.draw_x, tile_y+game.draw_y, 0)
 	 end
       end
    end
