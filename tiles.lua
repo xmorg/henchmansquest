8a2aae64375776d86f404dc507fc18bc87e9bce8
Tim <jobs@cooperlabs.net>
2015-05-25 13:46:47 -0700

    mod

--- tiles.lua
+++ tiles.lua
@@ -23,25 +23,28 @@ function on_load_tiles() --load the tiles
    --tiles
 
    
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile001.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile002.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile003.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile004.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile005.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile006.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile007.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile008.png") )
-   table.insert(tile_images,love.graphics.newImage("data/tiles/tile009.png") )
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile001.png") ) --wood floor
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile002.png") ) --wood floor reverse
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile003.png") ) --wood floor with rug
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile004.png") ) --stone floor
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile005.png") ) --stone floor cracked.
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile006.png") ) --stone floor darker
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile007.png") ) --leafy floor
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile008.png") ) --leafy floor(very)
+   table.insert(tile_images,love.graphics.newImage("data/tiles/tile009.png") ) --grassy floor
    table.insert(tile_images,love.graphics.newImage("data/tiles/tile999.png") )
    
    --walls
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall001.png") )
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall002.png") )
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall003.png") )
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall004.png") )
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall005.png") )
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall006.png") )
-   table.insert(tile_images,love.graphics.newImage("data/walls/cube_wall007.png") )
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall001.png") ) --wood with pillars
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall002.png") ) --wood wall
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall003.png") ) --wood wall (windows)
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall004.png") ) --stone wall
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall005.png") ) --tree
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall006.png") ) --tree lighter
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall007.png") ) --crate
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall008.png") ) --4 crates
+   table.insert(wall_images,love.graphics.newImage("data/walls/cube_wall009.png") ) --campfire
+   
 
    table.insert(tile_images,love.graphics.newImage("data/tiles/tile999.png") )
    
@@ -108,8 +111,8 @@ function draw_select_grid()
    love.graphics.push()	love.graphics.scale(game.zoom_level)
    for y = 1, game.tilecount do
       for x = 1, game.tilecount do
-	 tile_x = game.draw_x+(y + x) * 125 +250  --250 + 125
-	 tile_y = game.draw_y+(y - x) * 125 /2 + (129/2) --129 / 2 + 64
+	 tile_x = game.draw_x+(y + x) * 125 + (250)  --250 + 125
+	 tile_y = game.draw_y+(y - x) * 125 /2 +  ( 129/2 )  --129 / 2 + 64
 	 if y == game.tile_selected_y and x == game.tile_selected_x then
 	    love.graphics.draw(tile_images[last_tile], tile_x+game.draw_x, tile_y+game.draw_y, 0)
 	 end
